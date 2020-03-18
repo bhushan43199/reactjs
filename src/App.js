61b7970bffcef5809275fcd25a3f1f00bf7c5f94
montu patel <bhushanketanbhai.patel@calbaptist.edu>
2020-03-18 00:57:19 -0700

    first react app

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bhushan First React App
         </a>
       </header>
     </div>
